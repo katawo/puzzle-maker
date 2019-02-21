4a204b5675be6c492d80b9c11eeb10c1468e3de7
thangtk <thang.trinh@unicorn-fintech.com>
2019-02-21 16:13:37 +0700

    Updates

---
+++ precache-manifest.260feda38aadfd2a1da5dac2aa403904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2ae4139a0a3dc55b025",
+    "url": "/puzzle-maker/static/js/runtime~main.f2ae4139.js"
+  },
+  {
+    "revision": "13e9a8e594b89169db6b",
+    "url": "/puzzle-maker/static/js/main.13e9a8e5.chunk.js"
+  },
+  {
+    "revision": "d34f7877fa3d47dc475f",
+    "url": "/puzzle-maker/static/js/2.d34f7877.chunk.js"
+  },
+  {
+    "revision": "13e9a8e594b89169db6b",
+    "url": "/puzzle-maker/static/css/main.3853fb02.chunk.css"
+  },
+  {
+    "revision": "d34f7877fa3d47dc475f",
+    "url": "/puzzle-maker/static/css/2.9e6285de.chunk.css"
+  },
+  {
+    "revision": "e75fc6ae5feb61f9bff90c4a0d6161d6",
+    "url": "/puzzle-maker/index.html"
+  }
+];
