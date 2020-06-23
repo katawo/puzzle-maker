9cb16fe347f4debe00e43165bf2741787867dd38
kata <katawo@gmail.com>
2020-06-23 18:23:02 +0700

    init branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import logo from "./logo.svg";
-import "./App.css";
-import WordSearch from "./puzzles/WordSearch";
+import './App.css';
+// import WordSearch from './puzzles/WordSearch';
+import DailyPlan from './small-tools/DailyPlan';
+import { Container } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <WordSearch />
-        <br />
-        <br />
-      </div>
+      <Container maxWidth="md">
+        <div className="Daily-tasks">
+          {/* <WordSearch /> */}
+          <DailyPlan />
+          <br />
+          <br />
+        </div>
+      </Container>
     );
   }
 }
