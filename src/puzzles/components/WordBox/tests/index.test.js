e8ffd2c59051abbe92a1065641efca81525362e6
kata <katawo@gmail.com>
2020-06-23 15:18:52 +0700

    fix unit test in WordBox component

--- src/puzzles/components/WordBox/tests/index.test.js
+++ src/puzzles/components/WordBox/tests/index.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import WordBox from "../index";
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WordBox from '../index';
 
 configure({ adapter: new Adapter() });
 
-describe("<WordBox />", () => {
-  const component = shallow(<WordBox />);
-  it("Expect to have unit tests specified", () => {
+describe('<WordBox />', () => {
+  const component = shallow(<WordBox value={{}} />);
+  it('Expect to have unit tests specified', () => {
     expect(component.exists()).toEqual(true);
   });
 });
